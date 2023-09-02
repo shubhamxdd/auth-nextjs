dc2b167b2a09d4fd61b3d8ef9266e3a19f68bb25
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-09-02 22:38:24 +0530

    Create some routes

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <div>Login Page</div>
+  )
+}
+
+export default page
---
+++ app/signup/page.tsx
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <div>Signup Page</div>
+  )
+}
+
+export default page
