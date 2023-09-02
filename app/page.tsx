628980c958b9a0e6f5d423e38d368b83ebe0bbfa
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-09-02 23:23:17 +0530

    Add login and signup links on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold text-4xl mx-24">Aien</h1>
+      <div className="mt-3 mx-10">
+        <h1 className="font-bold text-4xl text-center mb-4">Aien</h1>
+        <div className="flex gap-5">
+          <Link
+            href={"/login"}
+            className="bg-blue-500 text-white rounded-md outline-none hover:bg-blue-600 py-2 px-4"
+          >
+            Login
+          </Link>
+          <br />
+          <Link
+            href={"/signup"}
+            className="bg-blue-500 text-white rounded-md outline-none hover:bg-blue-600 py-2 px-4"
+          >
+            SignUp
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
