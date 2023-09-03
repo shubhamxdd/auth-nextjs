c0c89eb449162056e97a85066504a848516fe9ec
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-09-03 17:57:40 +0530

    Creake login function

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,37 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast/headless";
 
-const page = () => {
+const LoginPage = () => {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
+  const [disabledBtn, setDisabledBtn] = useState(false);
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setDisabledBtn(false);
+    } else {
+      setDisabledBtn(true);
+    }
+  }, [user]);
+
+  const onLogin = async () => {
+    try {
+      const res = await axios.post("/api/users/login", user);
+      console.log("success login", res.data);
+      toast.success("Login Success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("error login", error.message);
+      toast.error(error.message);
+    }
+  };
   return (
     <>
       <div className="mt-4 mx-10">
@@ -45,7 +69,9 @@ const page = () => {
             />
             <button
               type="submit"
-              className="bg-blue-500 text-white rounded-md outline-none hover:bg-blue-600 px-4 py-2 -pb-2"
+              onClick={() => onLogin()}
+              disabled={disabledBtn ? true : false}
+              className="bg-blue-500 text-white rounded-md outline-none hover:bg-blue-600 disabled:bg-slate-600 px-4 py-2 -pb-2"
             >
               Login
             </button>
@@ -59,4 +85,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default LoginPage;
