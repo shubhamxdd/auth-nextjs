1333df11729690f4f6638adace17b31fd6e5a605
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-09-03 17:04:20 +0530

    Creaete and add user to database

---
+++ app/api/users/signup/route.ts
@@ -0,0 +1,44 @@
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import connectToDB from "@/utils/dbconfig";
+
+connectToDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+
+    console.log(reqBody);
+
+    // if user exists
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists!" },
+        { status: 400 }
+      );
+    }
+
+    // hash the password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    // create new user
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    const savedUser = await newUser.save();
+
+    console.log(savedUser);
+
+    return NextResponse.json({
+      message: "User created successfully!",
+      success: true,
+      savedUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
