a6af7344ad78fc42eb020e1b81435ed0b6c89309
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-09-04 15:29:52 +0530

    Add login functionality

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,32 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast/headless";
 
 const page = () => {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logged out successfully");
+      router.push("/");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error("Error logging out ", error.message);
+    }
+  };
   return (
     <>
       <div className="mx-10 mt-4">
-        <h1 className="text-center text-4xl font-bold">Profile Page</h1>
+        <button
+          className=" bg-black text-white rounded px-3 py-1 float-right"
+          onClick={logout}
+        >
+          Log Out
+        </button>
+        <h1 className="text-center text-4xl font-bold mx-16">Profile Page</h1>
         <div className="justify-center items-center flex">
           <Link
             href={"/"}
