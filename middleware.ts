602493ffea18c7acabaa206cdfe25ed3614081d0
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-09-04 16:29:48 +0530

    Create function to get user data from token

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isPublicPath = path === "/login" || path === "/signup";
 
-  const token = request.cookies.get("token") || "";
+  const token = request.cookies.get("token")?.value || "";
 
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL("/profile", request.nextUrl));
@@ -16,7 +15,6 @@ export function middleware(request: NextRequest) {
   }
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: ["/", "/login", "/signup", "/profile"],
 };
